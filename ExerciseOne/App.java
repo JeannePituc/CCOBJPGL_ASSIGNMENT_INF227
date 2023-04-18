021bc67ce4b41e944aa9d9e80743b9b6c3435102
Jeanne Marquel A. Pituc <119906684+JeannePituc@users.noreply.github.com>
2023-04-18 17:52:54 +0800

    Update App.java

--- ExerciseOne/App.java
+++ ExerciseOne/App.java
@@ -6,12 +6,17 @@ public class App {
 
         App myApp = new App();
 
+        //precondition (before the exam)
         assert iWillReview == true: "didn't review";
+         //assert iWillReview == true (promise to review before the exam)
+        //"didn't review" message if the promise is unfulfilled 
        
         myApp.exam();
 
+        //postcondition (after the exam)
         assert iWillNotFail == true: "low score";
-              
+         //assert iWillNotFail == true (promise to not fail after the exam)
+        // "low score" message if the promise is unfulfilled after the exam
     }
 
     void exam() {
